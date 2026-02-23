6100123e96d7dc859e8444b36530cf6267594eb7
Your Name <jahedulislamapurbo@gmail.com>
2026-02-23 16:30:45 +0600

    update:Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
+import Image from "next/image";
+import thumb from "../public/images/pngtree-d-gaming-laptop.jpg";
+
 export default function Home() {
   return (
     <div className="mt-10">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, similique?
-      Fugit labore fugiat beatae aspernatur pariatur unde earum sapiente
-      adipisci voluptatem aliquam recusandae quos ut ipsam excepturi, voluptate
-      laborum quaerat?
+      <Image
+        placeholder="blur"
+        className="aspect-video rounded-2xl mb-7"
+        src={thumb}
+        alt="thumb image"
+      />
+
+      <p className="text-base font-semibold ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, similique?
+        Fugit labore fugiat beatae aspernatur pariatur unde earum sapiente
+        adipisci voluptatem aliquam recusandae quos ut ipsam excepturi,
+        voluptate laborum quaerat?
+      </p>
     </div>
   );
 }
